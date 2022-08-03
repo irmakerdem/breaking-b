94574740f60a4bfc2a59f60e6cce3caab7f6b22b
Irmak Erdem <irmakerdem9@gmail.com>
2022-08-03 12:31:26 -0600

    Add Heisenberg icon to browser tab, set up basic React and Browser Router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>HI IRMAKKKK!!! 🥳</p>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      ideas: []
+    }
+  }
+
+  render() {
+    return(
+      <main className='App'>
+        <h1>Breaking Bad</h1>
+        <p></p>
+      </main>
+    )
+  }
 }
 
 export default App;
