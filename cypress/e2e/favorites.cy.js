e53344d0669669e2ffad12f4c9cb18b22d98f1d2
Irmak Erdem <irmakerdem9@gmail.com>
2022-08-06 22:40:30 -0600

    Adjust beforeEach hook for Favorites page testing

--- cypress/e2e/favorites.cy.js
+++ cypress/e2e/favorites.cy.js
@@ -1,25 +1,32 @@
 describe('Favorites Page', () => {
   beforeEach(() => {
     cy.intercept('GET', 'https://breakingbadapi.com/api/characters?category=Breaking+Bad', {fixture : 'getMockData.json'});
-    cy.visit('http://localhost:3000/favorites');
+    cy.visit('http://localhost:3000');
+    cy.get(':nth-child(4) > a > .character-image').click()
+    cy.get('.empty-heart').click();
+    cy.get('.go-favorites').click();
   })
 
   it('Should display favorited characters', () => {
+    cy.url().should('eq', 'http://localhost:3000/favorites');
     cy.get('h1').contains('Breaking Bad');
+    cy.get('h2').contains('Favorite Characters');
     cy.get('.go-home').contains('GO HOME');
-    cy.contains('Lydia Rodarte-Quayle').should('be.visible');
+    cy.get('.app-main').find('.card').should('have.length', '1')
+    cy.contains('Carmen Molina').should('be.visible');
   })
 
-  it('Should be able to go back to details page when clicking favorited character', () => {
-    cy.get('.empty-heart').click();
+  it('Should be able to go back to Details Page when clicking favorited character', () => {
+    cy.get('.character-image').click();
+    cy.url().should('eq', 'http://localhost:3000/Carmen%20Molina');
   })
 
-  it('Should be able to go back to the home page when clicking page title', () => {
+  it('Should be able to go back to the Home Page when clicking page title', () => {
     cy.get('h1').click();
 		cy.url().should('eq', 'http://localhost:3000/');
 	})
 
-  it('Should be able to go back to the home page when clicking GO HOME button', () => {
+  it('Should be able to go back to the Home Page when clicking GO HOME button', () => {
     cy.get('.go-home').click();
 		cy.url().should('eq', 'http://localhost:3000/');
 	})
