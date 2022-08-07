5dfa32f5f12fb30940cb34c8de59914ed09058f8
Irmak Erdem <irmakerdem9@gmail.com>
2022-08-06 22:01:30 -0600

    Update beforeEach hook to have character specific url

--- cypress/e2e/details.cy.js
+++ cypress/e2e/details.cy.js
@@ -2,13 +2,14 @@ describe('Details Page', () => {
   beforeEach(() => {
     cy.intercept('GET', 'https://breakingbadapi.com/api/characters?category=Breaking+Bad', {fixture : 'getMockData.json'});
     cy.visit('http://localhost:3000/');
-    // cy.visit('http://localhost:3000/Lydia%20Rodarte-Quayle');
+    cy.get(':nth-child(3) > a > .character-image').click()
   })
 
   it('Should display the title, two buttons, a heart, an image, and a list of character\s details', () => {
+    cy.url().should('eq', 'http://localhost:3000/Lydia%20Rodarte-Quayle');
     cy.get('h1').contains('Breaking Bad');
     cy.get('.go-favorites').contains('GO TO FAVORITES');
-    // cy.get('.go-home').contains('GO HOME');
+    cy.get('.go-home').contains('GO HOME');
     cy.get('.details-card-image').should('be.visible');
     cy.get('.empty-heart').should('be.visible');
     cy.get('.details-card').contains('Name: Lydia Rodarte-Quayle');
@@ -20,10 +21,14 @@ describe('Details Page', () => {
 
   it('Should be able to favorite a character', () => {
     cy.get('.empty-heart').click();
+    cy.get('.full-heart').should('be.visible');
   })
 
   it('Should be able to unfavorite a character', () => {
     cy.get('.empty-heart').click();
+    cy.get('.full-heart').should('be.visible');
+    cy.get('.full-heart').click();
+    cy.get('.empty-heart').should('be.visible');
   })
 
   it('Should be able to go back to the home page when clicking page title', () => {
@@ -36,8 +41,4 @@ describe('Details Page', () => {
 		cy.url().should('eq', 'http://localhost:3000/');
 	})
 
-  it('Should be able to go back to the home page when clicking GO TO FAVORITES button', () => {
-    cy.get('.go-favorites').click();
-		cy.url().should('eq', 'http://localhost:3000/');
-	})
 })
