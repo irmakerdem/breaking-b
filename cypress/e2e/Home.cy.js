2acd0223833abd0d5947ac0ec5c656dcf1547a0f
Irmak Erdem <irmakerdem9@gmail.com>
2022-08-06 21:32:13 -0600

    Add testing for Home page

---
+++ cypress/e2e/Home.cy.js
@@ -0,0 +1,24 @@
+describe('Home Page', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'https://breakingbadapi.com/api/characters', {fixture : 'getMockData.json'});
+    cy.visit('http://localhost:3000/');
+  })
+
+  it('Should display the title, a button, and a list of characters', () => {
+    cy.get('h1').contains('Breaking Bad');
+    cy.get('.go-favorites').contains('GO TO FAVORITES');
+    // cy.get('.home-container').should('have.length', '0');
+    // cy.get('.app-main').should('have.length', '1')
+    cy.contains('Bogdan Wolynetz').should('be.visible');
+    cy.get(':nth-child(2) > a > .character-image').should('be.visible');
+    cy.contains('Lydia Rodarte-Quayle').should('be.visible');
+    cy.get(':nth-child(3) > a > .character-image').should('be.visible');
+    cy.contains('Carmen Molina').should('be.visible');
+    cy.get(':nth-child(4) > a > .character-image').should('be.visible');
+  })
+
+  it('Should be able to go back to the home page when clicking page title', () => {
+    cy.get('h1').click();
+		cy.url().should('eq', 'http://localhost:3000/');
+	})
+ })
